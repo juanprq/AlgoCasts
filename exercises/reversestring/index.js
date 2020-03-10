e3f19faac3d0c72060d8bb9b1bf75524f4023f68
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-03-10 16:50:30 -0500

    Adds solution for the reverse string problem

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,40 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// Simple solution using the reverse method from array
+// const reverse = ([...list]) => list.reverse().join('');
+
+// More sofisticated solution using reduce
+// function reverse([...list]) {
+//   return list.reduce((accum, value) => {
+//     return value + accum;
+//   }, '');
+// }
+
+// Even a more sofisticated solution using recursion
+// const localReverse = ([head, ...rest]) => (head ? localReverse(rest) + head : '');
+// const reverse = ([...list]) => localReverse(list);
+
+// A solution using a for loop, very imperative
+// const reverse = (string) =>  {
+//   let result = '';
+
+//   for (let i = string.length - 1; i >= 0; i--) {
+//     result += string[i];
+//   }
+
+//   return result;
+// };
+
+// A little more cleaner solution using a more expressive for
+const reverse = (string) => {
+  let result = '';
+
+  for (let letter of string) {
+    result = letter + result;
+  }
+
+  return result;
+};
 
 module.exports = reverse;

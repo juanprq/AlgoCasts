493dc7bb9f990f9331427687ee36b37e4a36a885
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-03-10 20:36:49 -0500

    Adds solution for the maxchar problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,17 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar([...str]) {
+  const charCountMap = str.reduce(
+    (accum, char) => ({ ...accum, [char]: accum[char] + 1 || 1 }),
+    {},
+  );
+
+  return Object.keys(charCountMap).reduce((accum, char) => {
+    if (accum.value < charCountMap[char]) return { value: charCountMap[char], char };
+
+    return accum;
+  }, { char: '', value: -1 }).char;
+}
 
 module.exports = maxChar;

dc017080b43b6d6b15b27f89813c2ef50352c4c0
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-03-16 20:03:35 -0500

    Adds solution for the pyramid problem

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,7 +13,49 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//       '   #   '
+//       '  ###  '
+//       ' ##### '
+//       '#######'
 
-function pyramid(n) {}
+// function pyramid(n) {
+//   const matrix = [];
+//   const columns = n * 2 - 1;
+
+//   for (let i = 0; i < n; i++) {
+//     matrix[i] = [];
+
+//     for (let j = 0; j < columns; j++) {
+//       const pivot = Math.floor(columns / 2);
+//       if (j >= pivot - i && j <= pivot + i) {
+//         matrix[i][j] = '#';
+//       } else {
+//         matrix[i][j] = ' ';
+//       }
+//     }
+//   }
+
+//   matrix.map(row => console.log(row.join('')));
+// }
+
+const pyramid = (n, row = 0, level = '') => {
+  if (n === 1) {
+    console.log('#');
+    return;
+  }
+  if (row === n) return;
+
+  const maxLength = n * 2 - 1;
+  if (level.length >= maxLength) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const pivot = Math.floor(maxLength / 2);
+  const char = level.length >= pivot - row && level.length <= pivot + row ? '#' : ' ';
+
+  pyramid(n, row, level + char);
+};
 
 module.exports = pyramid;

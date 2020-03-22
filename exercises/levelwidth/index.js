3f87a61f75613b9c49ce35368256974765cae5d2
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-03-22 14:59:02 -0500

    Adds solution for the level width problem

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,26 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+const STOP = 'STOP';
+function levelWidth(root) {
+  let holder = [root, STOP];
+  const levelCounter = [0];
+
+  while(holder.length) {
+    const [head, ...rest] = holder;
+
+    if (head === STOP && !rest.length) {
+      holder = [...rest];
+    } else if (head === STOP) {
+      levelCounter.push(0);
+      holder = [...rest, STOP];
+    } else {
+      holder = [...rest, ...head.children];
+      levelCounter[levelCounter.length - 1] = levelCounter[levelCounter.length - 1] + 1;
+    }
+  }
+
+  return levelCounter;
+}
 
 module.exports = levelWidth;

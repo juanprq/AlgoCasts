f61a498d0b123b204dd613e88a616b6dfc98246d
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-03-24 21:55:31 -0500

    Adds solution for the validate binary search tree problem

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,22 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+const valueInRange = (value, min, max) => {
+  if (min === null && max === null) return true;
+  if (min === null) return value <= max;
+  if (max === null) return value >= min;
+
+  return min <= value && value <= max;
+}
+
+function validate(node, min = null, max = null) {
+  if (node === null) {
+    return true;
+  }
+
+  return valueInRange(node.data, min, max)
+    && validate(node.left, min, node.data)
+    && validate(node.right, node.data, max);
+}
 
 module.exports = validate;
